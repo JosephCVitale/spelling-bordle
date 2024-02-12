41fa4aa04da4732ca187fb758984db2525b00b19
JosephCVitale <37516659+JosephCVitale@users.noreply.github.com>
2024-02-12 01:44:07 -0600

    update hard coded honeycomb vals

--- vite-app/src/components/Honeycomb.jsx
+++ vite-app/src/components/Honeycomb.jsx
@@ -4,23 +4,23 @@ const Honeycomb = () => {
     return ( 
         <div className="honeycomb">
             <div>
-                <div className="hexagon">J</div>
+                <div className="hexagon">A</div>
             </div>
             <div>
-                <div className="hexagon">M</div>
+                <div className="hexagon">C</div>
                 <div className="emptyspace"></div>
-                <div className="hexagon">N</div>
+                <div className="hexagon">H</div>
             </div>
             <div>
                 <div className="hexagon required">I</div>
             </div>
             <div>
-                <div className="hexagon">A</div>
+                <div className="hexagon">L</div>
                 <div className="emptyspace"></div>
-                <div className="hexagon">G</div>
+                <div className="hexagon">R</div>
             </div>
             <div>
-                <div className="hexagon">U</div>
+                <div className="hexagon">C</div>
             </div>
         </div>
 
