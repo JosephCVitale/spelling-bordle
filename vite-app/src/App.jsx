179da7ac644e5b47d5e735446bb8123e2b60a69e
JosephCVitale <37516659+JosephCVitale@users.noreply.github.com>
2024-02-12 20:11:58 -0600

    add spelling bee logic and generation

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,26 +1,81 @@
-import { useState, useEffect} from 'react'
-import './App.css'
-import Grid from './components/Grid'
-import Honeycomb from './components/Honeycomb'
+import { useState, useEffect} from 'react';
+import './App.css';
+import Grid from './components/Grid';
+import Honeycomb from './components/Honeycomb';
+import dictionary from './data-files/dictionary.json'; 
 
 function App() {
-  
-  const eventfulKeys = [
-    "Backspace", "Enter",
-    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
-  ];
-
-  // TODO: replace hard coded values with randomly generated
-  const validWords = [ "avail", "chair", "chica", "chili", "chill", "cilia", "circa", "civic", "civil", "lilac", "rival", "villa", "viral"];
-  const answer = 'viral';
-
+  const requiredLetterIndex = 3;
   const [guess, setGuess] = useState('');
   const [prevGuesses, setPrevGuesses] = useState([]);
   const [col, setCol] = useState(0);
   const [row, setRow] = useState(0);
   const [win, setWin] = useState(false);
   const [lose, setLose] = useState(false);
+  const [honeycomb, setHoneycomb] = useState([]);
+  const [validWords, setValidWords] = useState([]);
+  const [answer, setAnswer] = useState('');
+  
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function generateHoneycomb(){
+    let hc = [];
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    while (hc.length < 7){
+      let randomIndex =  getRandomInt(alphabet.length);
+      hc.push(alphabet[randomIndex]);
+      alphabet.splice(randomIndex, 1); 
+    }
+    return hc;
+  }
+
+  function pickAnswer(vWords){
+    let randomIndex =  getRandomInt(vWords.length);
+    return vWords[randomIndex];
+  }
+
+  useEffect(() => {
+    let hc = generateHoneycomb();
+    let reqLetter = hc[requiredLetterIndex];
+    let vWords = [];
+    let validHoneycomb = false;
+    while(validHoneycomb == false) {
+      for (let word in dictionary){
+        if(word.length == 5 && word.includes(reqLetter)){
+          let check = word
+          for (let letter of hc){
+            check = check.replaceAll(letter, '');
+          }
+          if(check.length == 0){
+            vWords.push(word);
+          }
+        }
+      }
+      if(vWords.length >= 100){
+        validHoneycomb = true;
+      } else {
+        hc = generateHoneycomb();
+        reqLetter = hc[requiredLetterIndex];
+        vWords = [];
+      }
+    }
+    const ans = pickAnswer(vWords);
+    setHoneycomb(hc);
+    setValidWords(vWords);
+    setAnswer(ans);
+    console.log(hc);
+    console.log(reqLetter);
+    console.log(vWords);
+    console.log(ans);
+  }, []); 
+
+  const eventfulKeys = [
+    "Backspace", "Enter",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+  ];
 
   useEffect(() => {
     const handleKeyPress = (event) => {
@@ -51,7 +106,7 @@ function App() {
         } else {
           //alert('Invalid word');
         }
-      } else if (guess.length < 5){
+      } else if (guess.length < 5 && event.key != 'Enter'){
         const newGuess = guess + event.key.toLowerCase();
         setGuess(newGuess);
         setCol(col => col + 1);
@@ -63,29 +118,13 @@ function App() {
     };
   }, [guess, col, row, prevGuesses]);
 
-  // TODO: remove, for temporary logging purposes
-  /*
-  useEffect(() => {
-    console.log('Most recent guess:', guess);
-  }, [guess]);
-  useEffect(() => {
-    console.log('col:', col);
-  }, [col]);
-  useEffect(() => {
-    console.log('row:', row);
-  }, [row]);
-  useEffect(() => {
-    prevGuesses.forEach((element) => console.log(element));
-  }, [prevGuesses]);
-  */
-
   return (
     <div className="App">
       <nav>
         <h1>Spelling Bordle</h1>
       </nav>
-      <Grid guess = {guess} col = {col} row = {row} prevs = {prevGuesses} ans = {answer} />
-      <Honeycomb />
+      <Grid guess={guess} col={col} row={row} prevs={prevGuesses} ans={answer} />
+      <Honeycomb honeycomb={honeycomb}/>
     </div>
   )
 }
--- vite-app/src/components/Honeycomb.jsx
+++ vite-app/src/components/Honeycomb.jsx
@@ -1,26 +1,35 @@
 import '../App.css'
 
-const Honeycomb = () => {
+const Honeycomb = ({honeycomb}) => {
+
+    const hexOne = honeycomb[0]?.toUpperCase();
+    const hexTwo = honeycomb[1]?.toUpperCase();
+    const hexThree = honeycomb[2]?.toUpperCase();
+    const hexFour = honeycomb[3]?.toUpperCase();
+    const hexFive = honeycomb[4]?.toUpperCase();
+    const hexSix = honeycomb[5]?.toUpperCase();
+    const hexSeven = honeycomb[6]?.toUpperCase();
+
     return ( 
         <div className="honeycomb">
             <div>
-                <div className="hexagon">A</div>
+                <div className="hexagon">{hexOne}</div>
             </div>
             <div>
-                <div className="hexagon">C</div>
+                <div className="hexagon">{hexTwo}</div>
                 <div className="emptyspace"></div>
-                <div className="hexagon">H</div>
+                <div className="hexagon">{hexThree}</div>
             </div>
             <div>
-                <div className="hexagon required">I</div>
+                <div className="hexagon required">{hexFour}</div>
             </div>
             <div>
-                <div className="hexagon">L</div>
+                <div className="hexagon">{hexFive}</div>
                 <div className="emptyspace"></div>
-                <div className="hexagon">R</div>
+                <div className="hexagon">{hexSix}</div>
             </div>
             <div>
-                <div className="hexagon">V</div>
+                <div className="hexagon">{hexSeven}</div>
             </div>
         </div>
 
