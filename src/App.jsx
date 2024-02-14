cbfd8c7af3c8b13cdf472d59b07fa360563b12e6
JosephCVitale <37516659+JosephCVitale@users.noreply.github.com>
2024-02-13 23:29:34 -0600

    tweaks and alerts

--- src/App.jsx
+++ src/App.jsx
@@ -18,60 +18,56 @@ function App() {
   const [win, setWin] = useState(false);
   const [lose, setLose] = useState(false);
 
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-
   function generateHoneycomb(){
     let hc = [];
     let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     while (hc.length < 7){
-      let randomIndex =  getRandomInt(alphabet.length);
+      let randomIndex =  Math.floor(Math.random() * alphabet.length);
       hc.push(alphabet[randomIndex]);
       alphabet.splice(randomIndex, 1); 
     }
     return hc;
   }
 
-  function pickAnswer(vWords){
-    let randomIndex =  getRandomInt(vWords.length);
-    return vWords[randomIndex];
+  function pickAnswer(words){
+    let randomIndex =  Math.floor(Math.random() * words.length);
+    return words[randomIndex];
   }
 
   useEffect(() => {
     let hc = generateHoneycomb();
-    let reqLetter = hc[requiredLetterIndex];
-    let vWords = [];
+    let rl = hc[requiredLetterIndex];
+    let vw = [];
     let validHoneycomb = false;
     while(validHoneycomb == false) {
       for (let word in dictionary){
-        if(word.length == 5 && word.includes(reqLetter)){
-          let check = word
+        if(word.length == 5 && word.includes(rl)){
+          let remainingLetters = word
           for (let letter of hc){
-            check = check.replaceAll(letter, '');
+            remainingLetters = remainingLetters.replaceAll(letter, '');
           }
-          if(check.length == 0){
-            vWords.push(word);
+          if(remainingLetters.length == 0){
+            vw.push(word);
           }
         }
       }
-      if(vWords.length >= 20){
+      if(vw.length >= 20){
         validHoneycomb = true;
       } else {
         hc = generateHoneycomb();
-        reqLetter = hc[requiredLetterIndex];
-        vWords = [];
+        rl = hc[requiredLetterIndex];
+        vw = [];
       }
     }
-    const ans = pickAnswer(vWords);
+    const ans = pickAnswer(vw);
     setHoneycomb(hc);
-    setValidWords(vWords);
+    setValidWords(vw);
     setAnswer(ans);
     
     console.log('You are cheating!')
     console.log('honeycomb:', hc);
-    console.log('required letter:', reqLetter);
-    console.log('valid words:', vWords);
+    console.log('required letter:', rl);
+    console.log('valid words:', vw);
     console.log('answer:', ans);
   }, []); 
 
@@ -97,7 +93,7 @@ function App() {
         setCol(0)
         setPrevGuesses(prevGuesses => [...prevGuesses, guess]);
         setWin(true);
-        //alert("You win!");
+        alert("You win!");
       } else if (validWords.includes(guess) && !prevGuesses.includes(guess)){
         setRow(row => row+1);
         setPrevGuesses(prevGuesses => [...prevGuesses, guess]);
@@ -105,17 +101,16 @@ function App() {
         setCol(0);
         if(row == 5){
           setLose(true);
-          //alert("You lose!");
+          alert("You lose!");
         } 
       } else {
-        //alert('Invalid word');
+        alert('Invalid guess');
       }
     } else if (guess.length < 5 && input != 'enter'){
       const newGuess = guess + input.toLowerCase();
       setGuess(newGuess);
       setCol(col => col + 1);
     }
-
   }
 
   useEffect(() => {
@@ -127,14 +122,6 @@ function App() {
       document.removeEventListener('keydown', handleKeyPress);
     };
   }, [guess, col, row, prevGuesses]);
-
-  const handleHoney = (hexValue) => {
-    if(hexValue == 'shuffle'){
-      shuffle();
-    } else {
-      handleInput(hexValue);
-    }
-  }
 
   function shuffle() {
     const requiredLetter = honeycomb[requiredLetterIndex];
@@ -158,15 +145,23 @@ function App() {
     setHoneycomb(shuffledHoneycomb);
   }
 
+  const handleHoney = (hexValue) => {
+    if(hexValue == 'shuffle'){
+      shuffle();
+    } else {
+      handleInput(hexValue);
+    }
+  }
+
   return (
     <div className="App">
       <nav>
         <h1>Spelling Bordle</h1>
       </nav>
-      <Grid guess={guess} col={col} row={row} prevs={prevGuesses} ans={answer} />
+      <Grid guess={guess} col={col} row={row} prevs={prevGuesses} ans={answer}/>
       <Honeycomb honeycomb={honeycomb} handleHoney={handleHoney}/>
     </div>
-  )
+  );
 }
 
 export default App;
